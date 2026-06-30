88fadb9d42b09419838a68ecb55ec15ac21cddbd
Bhuwan Jain <91060523+B200001@users.noreply.github.com>
2026-06-30 09:53:23 +0530

    Add: subsets - submission-8

---
+++ Data Structures & Algorithms/subsets/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        curr = []
+        def solve(start):
+            res.append(curr.copy())
+            for i in range(start, len(nums)):
+                curr.append(nums[i])
+                solve(i +  1)
+                curr.pop()
+        solve(0)
+        return res
